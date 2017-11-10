1fbf58f489fb07b46b6ab8c658f8b8372345f038
Ajit Rana <ajit.thakurs210@gmail.com>
2017-11-10 20:59:00 +0530

    init project done

---
+++ Website/index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Todo App</title>
+        <meta name="description" content="A reminder list management">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="manifest" href="site.webmanifest">
+        <!-- Place favicon.ico in the root directory -->
+		
+		<!-- external css libs -->
+        <link rel="stylesheet" href="pulbic/css/normalize.css">
+        <link rel="stylesheet" href="public/css/bootstrap.css">
+        <link rel="stylesheet" href="public/css/font-awesome/css/font-awesome.css">
+		
+		<!-- custom styles -->
+        <link rel="stylesheet" href="public/css/main.css">
+
+        <!-- external js libs -->
+        <script src="public/js/jquery.js"></script>
+    </head>
+    <body>
+       
+       <!-- external js libs -->
+       <script src="public/js/bootstrap.js"></script>
+		
+		<h4 class="text-center mt-5">Working Todo App</h4>
+
+       <!-- custom js -->
+       <script src="public/js/bootstrap.js" async="true"></script>
+    </body>
+</html>
