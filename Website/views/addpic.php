edec9aa9197c642cd4628a8eb4cc12fff1d2ce4f
Ajit Rana <ajit.thakurs210@gmail.com>
2017-11-11 23:01:32 +0530

    adding dyanamic file loader for css and js libs

---
+++ Website/views/addpic.php
@@ -0,0 +1,16 @@
+<?php
+	require_once("../controller/attachments.php");
+	$loader = new Loader();
+?>
+
+<!doctype html>
+<html class="no-js">
+    <head>
+        <title>Todo App | Profie Pic</title>
+        <?php $loader->head_load(); ?>
+    </head>
+    <body>
+		<!-- work to do for signup -->
+   	 	<?php $loader->body_load(); ?>
+    </body>
+</html>
