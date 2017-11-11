b255e2c29d366c44862f5a4df8e575e289e7cd36
ashishahujaa450 <ahuja3594@gmail.com>
2017-11-11 23:14:24 +0530

    profile page done also

--- Website/views/profile.php
+++ Website/views/profile.php
@@ -32,79 +32,47 @@
         <main class="main container mt-5 pt-5">
             <div class="row justify-content-center">
                 <div class="col-sm-7">
-
+                    
+                    <!--header content block-->
+                    <!--header content block-->
                     <div class="content-block profile-header w-100 text-right px-2 mb-5 py-1 row">
                         <div class="col-8 ">                            
                             <h3 class="font-1">Todo App <img src="../public/images/logo.png" alt=""></h3>
                         </div><!--end col 8-->
 
                         <div class="col-4">
-                            <h4><img src="../public/images/user.png" alt=""></h4>
+                            <p class="text-muted mb-0"><img src="../public/images/user.png" alt=""> Hi, Crakers <i class="fa fa-power-off"></i></p>
                         </div><!--end col 4-->   
                     </div><!--end profile header block-->
 
+                    
+                    <!--listing content block-->
+                    <!--listing content block-->
+                    <div class="content-block row w-100 text-center justify-content-center py-5">
+                        <div class="col-8">
+                            <i class="fa fa-plus" id="add"></i>
 
-                    <div class="content-block row w-100 text-center">
-                        <div class="w-100 login-form px-5 py-5">
-                            
-                            <hr>
+                            <div class="card priority-1 custom-card">
+                                <div class="card-body">
+                                    <h5>Home Work</h5>
+                                    <p>Complete data structure to answer my team.</p>
+                                </div><!--end card body-->
+                            </div><!--end card-->
 
-                            <form action="#" method="post">
+                            <div class="card priority-2 custom-card">
+                                <div class="card-body">
+                                    <h5>Happy bdy</h5>
+                                    <p>Coder bday is here don't forget to wish him on 2nd October.</p>
+                                </div><!--end card body-->
+                            </div><!--end card-->
 
-                                <div class="form-group mt-5">
-                                    <label for="fname" class="sr-only">full name</label>
-                                   <div class="input-group mb-2">
-
-                                       <div class="input-group-addon rounded-0"><i class="fa fa-user"></i></div>
-
-                                        <input type="password" class="form-control rounded-0" id="fname" placeholder="Full Name">
-
-                                   </div><!--end input group-->
-                                </div><!--end form group-->
-
-                                <div class="form-group">
-                                    <label for="email" class="sr-only">Email</label>
-                                   <div class="input-group mb-2">
-
-                                       <div class="input-group-addon rounded-0"><i class="fa fa-envelope"></i></div>
-
-                                        <input type="email" class="form-control rounded-0" id="email" placeholder="Email Address">
-
-                                   </div><!--end input group-->
-                                </div><!--end form group-->
-
-                                <div class="form-group">
-                                    <label for="pwd" class="sr-only">Password</label>
-                                   <div class="input-group mb-2">
-
-                                       <div class="input-group-addon rounded-0"><i class="fa fa-key"></i></div>
-
-                                        <input type="password" class="form-control rounded-0" id="pwd" placeholder="Password">
-
-                                   </div><!--end input group-->
-                                </div><!--end form group-->
-
-                                <div class="form-group">
-                                    <label for="mobile" class="sr-only">mobile</label>
-                                   <div class="input-group mb-2">
-
-                                       <div class="input-group-addon rounded-0"><i class="fa fa-mobile"></i></div>
-
-                                        <input type="text" class="form-control rounded-0" id="pwd" placeholder="Mobile">
-
-                                   </div><!--end input group-->
-                                </div><!--end form group-->
-
-                                <div class="form-group">
-                                    <div class="btn-group" role="group">
-                                        <button class="btn btn-primary btn-dark"><i class="fa fa-thumbs-o-up"></i></button>
-                                        <button class="btn btn-primary rounded-0">
-                                        Register With Todo
-                                        </button>
-                                    </div><!--end btn group-->
-                                </div><!--end form group-->
-                            </form><!--end form-->                        
-                        </div><!--end left col-->
+                            <div class="card priority-3 custom-card">
+                                <div class="card-body">
+                                    <h5>Main work</h5>
+                                    <p>Let's complete the Todo project work given to me.</p>
+                                </div><!--end card body-->
+                            </div><!--end card-->
+                        </div><!--end col 8-->
                     </div><!--end content block-->
                 </div><!--end col sm6-->
             </div><!--end row-->
